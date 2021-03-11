685342e6b464656881c95fdd82f484f030acd07e
Vasiliy Sotnikov <89111373320@mail.ru>
2021-03-11 19:03:45 +0300

    Добавил наследование и геттер

---
+++ chapter06/combomeal.php
@@ -0,0 +1,70 @@
+<?php
+require_once 'entree.php';
+
+class ComboMeal extends Entree {
+
+    public function __construct($name, $entrees)
+    {
+        parent::__construct($name, $entrees);
+        foreach ($entrees as $entree){
+            if (! $entree instanceof Entree){
+                throw new Exception ('Elements of $entrees must be Entree objects');
+            }
+        }
+    }
+
+    public function hasIngredient($ingredient): bool
+    {
+        foreach ($this->ingredients as $entree){
+            if ($entree->hasIngredient($ingredient)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+try {
+    //создать экземпляр и присвоить его переменной $soup
+    $soup = new Entree('Chiken Soup', array('chicken', 'water'));
+    //создать отдельный экземпляр и присвоить его переменной $sandwich
+    $sandwich = new Entree('Chiken Sandwich', array ('chicken', 'bread'));
+} catch (Exception $e) {
+    echo 'Couldn\'t create: ' . $e->getMessage();
+}
+
+foreach (['chiken', 'lemon', 'bread', 'water']  as $ing) {
+    if ($soup->hasIngredient($ing)){
+        echo "Soup contains $ing.</br>";
+    }
+    if ($sandwich->hasIngredient($ing)){
+        echo "Sandwich contains $ing.\n";
+    }
+}
+
+$sizes = Entree::getSizes();
+foreach($sizes as $size){
+    echo $size . " ";
+}
+
+try {
+    $drink = new Entree ('Glass of Milk', 'milk');
+    if ($drink->hasIngredient('milk')){
+        echo "Yummy!";
+    }
+} catch (Exception $e) {
+    echo "Couldn't create the drink: " . $e->getMessage();
+}
+
+// Составное блюдо
+try {
+    $combo = new ComboMeal('Soup + Sandwich', array($soup, $sandwich));
+} catch (Exception $e) {
+    echo 'Couldn\'t create: ' . $e->getMessage();
+}
+
+foreach (['chicken', 'water', 'pickles'] as $ing){
+    if ($combo->hasIngredient($ing)){
+        echo "Something in the combo contains $ing.\n";
+    }
+}
---
+++ chapter06/entree.php
@@ -0,0 +1,31 @@
+<?php
+class Entree {
+    private string $name;
+
+    protected array $ingredients = array();
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    function __construct($name, $ingredients){
+        if (!is_array($ingredients)){
+            throw new Exception('$ingredients must be array');
+        }
+        
+        $this->name = $name;
+        $this->ingredients = $ingredients;
+    }
+    public function hasIngredient($ingredient): bool
+    {
+        return in_array($ingredient, $this->ingredients);
+    }
+    public static function getSizes(): array
+    {
+        return array('small', 'medium', 'large');
+    }
+}
