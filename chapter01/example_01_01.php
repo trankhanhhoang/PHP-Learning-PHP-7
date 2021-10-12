e9a28b12e6161d0086902a4251c9e70d234b122c
Vasiliy Sotnikov <89111373320@mail.ru>
2021-10-12 21:57:32 +0300

    add example in chapter 01-03

---
+++ chapter01/example_01_01.php
@@ -0,0 +1,15 @@
+<!-- Пример 1.1. Здравствуй мир-->
+<!doctype html>
+<html>
+<head>
+    <title>PHP says hello</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <b>
+        <?php
+        print "Hello, World";
+        ?>
+    </b>
+</body>
+</html>
